6cf2338bbd26f8a3077781445dc73ae736c07186
Scott Harvey <scott@scottharvey.co>
2019-07-16 21:01:06 +0700

    Better blog layout

--- config.rb
+++ config.rb
@@ -31,11 +31,11 @@
 # Methods defined in the helpers block are available in templates
 # https://middlemanapp.com/basics/helper-methods/
 
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
+helpers do
+  def markdown(content)
+    Tilt['markdown'].new { content }.render
+  end
+end
 
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
